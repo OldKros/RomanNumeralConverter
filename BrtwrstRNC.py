f57db03d2ce95ed8504e6201441ba199b9ed8212
Krostik <47256571+OldKros@users.noreply.github.com>
2019-09-27 18:10:27 +0100

    Create BrtwrstRNC.py
    All Credit for this version goes to Brtwrst from the Engineer Man discord

---
+++ BrtwrstRNC.py
@@ -0,0 +1,10 @@
+# Credit to Brtwrst from the Engineer Man discord for turning my 76 line program into an 8 liner.
+
+conversion_lookup = (
+    ('', 'M', 'MM', 'MMM'),
+    ('', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'),
+    ('', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'),
+    ('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX')
+)
+num = '73'
+print(''.join([conversion_lookup[i][int(n)] for i,n in enumerate(num.zfill(4))]))
